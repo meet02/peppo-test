e549b985906a6f777409f4f8430ead4b1b88b3c7
meet02 <47363880+meet02@users.noreply.github.com>
2021-05-17 11:47:29 +0530

    Update db-connection.js

--- utils/db-connection.js
+++ utils/db-connection.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const func = require("../function");
 let confi = require("../config");
 
-let liveDbUrl =
-  "mongodb://doodle:J4nU6Q2vzDTu4gM4@cluster0-shard-00-00.yttgz.mongodb.net:27017,cluster0-shard-00-01.yttgz.mongodb.net:27017,cluster0-shard-00-02.yttgz.mongodb.net:27017/peppo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+let liveDbUrl ="";
 let localDbUrl = "mongodb://localhost:27017/peppo";
 
 mongoose
-  .connect(liveDbUrl, {
+  .connect(localDbUrl, {
     useNewUrlParser: true,
   })
   .then((result) => {
